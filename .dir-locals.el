d7368d51f975b4a841d7eb67e9e0b193044bc1f7
Vitalie Spinu <spinuvit@gmail.com>
2020-01-01 21:24:56 +0100

    Add dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,13 @@
+
+((nil
+  (require-final-newline . t)
+  ;; not tabs in code
+  (indent-tabs-mode)
+  ;; remove trailing whitespace
+  (comment-add . 0)
+  (eval . (add-hook 'before-save-hook 'delete-trailing-whitespace nil t)))
+ (makefile-gmake-mode
+  (indent-tabs-mode . t))
+ (ess-mode
+  (ess-fancy-comments)
+  (ess-indent-offset . 2)))
